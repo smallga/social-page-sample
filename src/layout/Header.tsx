a28f1a4ff50d9ecdebda22903b39cf0a3b4fa1ab
smallgag <smallgag@gmail.com>
2023-02-16 15:35:55 +0800

    1.修改樣式

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 }
 export default function SerachBarSerachBarSerachBar(props: HeaderProps) {
   return (
-    <div className="fixed w-full top-0 left-0 h-14 m-auto justify-center items-center border-b-2 border-slate-300 hidden lg:flex" >
+    <div className="fixed bg-white w-full p-0 left-0 h-14 m-auto justify-center items-center border-b-2 border-slate-300 hidden lg:flex" >
         <SerachBar></SerachBar>
         
     </div>
